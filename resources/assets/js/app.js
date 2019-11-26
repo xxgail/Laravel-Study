ef5cde6be9a788e459117772a7b0133b0845d5ad
Gail <xxxggggail@gmail.com>
2019-11-26 11:59:42 +0800

    laravel-vue-tailwind todo test

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,8 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+Vue.component('todo-list', require('./components/todo-list.vue'));
+
 const app = new Vue({
     el: '#app'
 });
